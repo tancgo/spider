b6d5d6442f0088cb7162c2f5e38919875c567f51
chao.tan01 <chao.tan01@liulishuo.com>
2021-01-26 19:26:37 +0800

    update

--- src/crowler.ts
+++ src/crowler.ts
@@ -1,7 +1,11 @@
 import superagent from "superagent";
 import cheerio from "cheerio";
 
-class Crowler {
+interface Course {
+  title: string;
+  count?: string; // 没有权限页面显示不出来count
+}
+class Crowller {
   private url = "http://www.dell-lee.com/";
 
   constructor() {
@@ -11,13 +15,24 @@ class Crowler {
   getJsonInfo(html: string) {
     const $ = cheerio.load(html);
     const courseItems = $(".course-item");
+    const courseInfos: Course[] = [];
 
     courseItems.map((index, item) => {
-        const descs = $(item).find(".course-desc")
-        const title = descs.eq(0).text();
-    console.log(title);
+      const descs = $(item).find(".course-desc");
+      const title = descs.eq(0).text();
 
-    })
+      courseInfos.push({
+        title,
+      });
+    });
+    // console.log(courseItems.length)
+
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos,
+    };
+
+    console.log(result);
   }
 
   async getRawHtml() {
@@ -27,4 +42,4 @@ class Crowler {
   }
 }
 
-const crowler = new Crowler();
+const crowller = new Crowller();
